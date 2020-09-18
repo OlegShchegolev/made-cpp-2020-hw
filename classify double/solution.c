5dee0fc1f542d8805877dfa76f3b86aae1685160
Oleg Shchegolev <shchegolev_oleg@mail.ru>
2020-09-18 03:38:08 +0300

    solution for classify_double

--- classify double/solution.c
+++ classify double/solution.c
@@ -15,7 +15,7 @@ uint64_t convertToUint64 (double number) {
 }
 
 bool getBit (const uint64_t number, const uint8_t index) {
-    /// Your code here...
+    return (number & (1 << index));
 }
 
 
@@ -24,7 +24,7 @@ bool getBit (const uint64_t number, const uint8_t index) {
  */
 
 bool checkForPlusZero (uint64_t number) {
-    /// Your code here.
+    return number == 0;
 }
 
 bool checkForMinusZero (uint64_t number) {
@@ -32,35 +32,39 @@ bool checkForMinusZero (uint64_t number) {
 }
 
 bool checkForPlusInf (uint64_t number) {
-    /// Your code here.
+    return number == 0x7FF0000000000000;
 }
 
 bool checkForMinusInf (uint64_t number) {
-    /// Your code here.
+    return number == 0xFFF0000000000000;
 }
 
 bool checkForPlusNormal (uint64_t number) {
-    /// Your code here.
+    return (number >= 0x0010000000000000 && \
+        number < 0x7FF0000000000000);
 }
 
 bool checkForMinusNormal (uint64_t number) {
-    /// Your code here.
+    return (number >= 0x8010000000000000 && \
+        number < 0xFFF0000000000000);
 }
 
 bool checkForPlusDenormal (uint64_t number) {
-    /// Your code here.
+    return (number > 0 && number < 0x0010000000000000);
 }
 
 bool checkForMinusDenormal (uint64_t number) {
-    /// Your code here.
+    return (number > 0x8000000000000000 && number < 0x8010000000000000);
 }
 
 bool checkForSignalingNan (uint64_t number) {
-    /// Your code here.
+    return (number > 0x7FF00000000000 && \
+        number < 0x7FF8000000000000);
 }
 
 bool checkForQuietNan (uint64_t number) {
-    /// Your code here.
+    return (number < 0x8000000000000000 && \
+        number >= 0x7FF8000000000000);
 }
 
 
